3627d6a4552df1ca72d78bf87b6b21d5ca9ea269
reharshD <reharsh_deshpande@persistent.com>
2023-01-16 17:30:37 +0530

    Add delete functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,7 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
     def edit 
-        @article = Article.find(params[:id])
+        
     end
     def update
         @article =  Article.find(params[:id])
@@ -30,6 +30,11 @@ class ArticlesController < ApplicationController
         else
             render 'new'
         end
+    end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
     end
 
 end
