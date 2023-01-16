c4aefbfce6d43c0fddff27d95a481ff237e2739a
reharshD <reharsh_deshpande@persistent.com>
2023-01-16 11:24:13 +0530

    Implementing show functionality in our demo

---
+++ app/models/article.rb
@@ -0,0 +1,4 @@
+class Article < ApplicationRecord
+    validates :title, presence: true , length: {minimum: 6, maximum: 100}
+    validates :desc, presence: true ,  length: {minimum: 10, maximum: 100}
+end
