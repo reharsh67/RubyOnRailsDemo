d22cc5e3f8fcd10abada8d48110d593ebbddfb66
reharshD <reharsh_deshpande@persistent.com>
2023-01-17 20:38:09 +0530

    create associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
     validates :title, presence: true , length: {minimum: 6, maximum: 100}
     validates :desc, presence: true ,  length: {minimum: 10, maximum: 100}
+    belongs_to :user
 end
