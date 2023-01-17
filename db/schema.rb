1ddd916dac57aae9a92e5fc182b92976c8ea510b
reharshD <reharsh_deshpande@persistent.com>
2023-01-17 20:53:56 +0530

    a bit of cleanup

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_17_124037) do
+ActiveRecord::Schema.define(version: 2023_01_17_152202) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "desc"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer "user_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
