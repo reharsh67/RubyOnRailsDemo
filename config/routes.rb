44c1ef6a882d97d0384d5c4215cf00d117d006ef
reharshD <reharsh_deshpande@persistent.com>
2023-01-18 15:37:54 +0530

    add user edit functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :users
   root "articles#home"
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, expect: [:new]
+  
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
