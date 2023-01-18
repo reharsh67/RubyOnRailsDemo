70e2c0243c2044d686bb1a4e00a3eb57f5066aaa
reharshD <reharsh_deshpande@persistent.com>
2023-01-18 18:35:38 +0530

    Added login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,13 @@ class UsersController < ApplicationController
             render 'edit'
         end
     end
-
+    def show
+        @user = User.find(params[:id])
+        @articles = @user.articles
+    end
+    def index
+        @users = User.all
+    end
     private
     def user_params
         params.require(:user).permit(:username, :email, :password)
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :articles
   get 'signup', to: 'users#new'
   resources :users, expect: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to:'sessions#create'
+  post 'logout', to:'sessions#destroy'
   
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
