305d9b3f200ec5ed1159b919eae661897f2464ae
reharshD <reharsh_deshpande@persistent.com>
2023-01-18 18:56:01 +0530

    add session and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,20 @@ class SessionsController < ApplicationController
     def new
     end
     def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate( params[:session][:password])
+            session[:user_id] = user.id
+            flash[:notice] = "Login sucess"
+            redirect_to user
+        else
+            flash.now[:alert ]= "Wrong details"
+            render 'new'
+        end
+
     end
     def destroy
+        session[:user_id] = nil
+        flash[:notice] = "Logged out"
+        redirect_to root_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   resources :users, expect: [:new]
   get 'login', to: 'sessions#new'
   post 'login', to:'sessions#create'
-  post 'logout', to:'sessions#destroy'
-  
-  
+  delete 'logout', to:'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
